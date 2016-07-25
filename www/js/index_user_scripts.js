483d2202813179470cf5b5f46b9f9d7a7a0fa91b
Adroaldo Pagliari <adroaldo@outlook.com>
2016-07-25 09:34:35 -0300

    Alterações na sincronização e comunicação com o WS.

--- www/js/index_user_scripts.js
+++ www/js/index_user_scripts.js
@@ -198,19 +198,15 @@ var padAssinatura;
         });
 
 
-        /* button  #btnsincronizar */
-        $(document).on("click", "#btnsincronizar", function (evt) {
-            var $loading = $('#loading').hide();
-            $(document)
-                .ajaxStart(function () {
-                    $loading.show();
-                    setInterval(function () {}, 10000);
-                })
-                .ajaxStop(function () {
-                    $loading.hide();
-                });
-            return false;
-        });
+        var $loading = $('#loading').hide();
+        $(document)
+            .ajaxStart(function () {
+                $loading.show();
+                setInterval(function () {}, 10000);
+            })
+            .ajaxStop(function () {
+                $loading.hide();
+            });
 
     }
     document.addEventListener("app.Ready", register_event_handlers, false);
--- www/js/ws.js
+++ www/js/ws.js
@@ -1,28 +1,50 @@
 $(function () {
-    // example call
-    /* button  #btntrabalhos */
-    $(document).on("click", "#btnsincronizar", function (evt) {
-        $.ajaxSetup({
-            async: true
-        }); // chamada é síncrona, estamos aguardando retorno
-        $.ajax({
-            type: 'GET',
-            // adaptar para sincronização
-            url: 'http://rasystems.esy.es/index.php/trabalhos',
-            dataType: 'json',
-            contentType: 'application/json; charset=utf-8',
-            beforeSend: function () {
-                $('#loading').show();
-            },
-            complete: function () {
-                //$("#loading").hide();
-            },
-            success: function (response) {
-                alert(JSON.stringify(response));
-            },
-            error: function (error) {
-                console.log(error);
-            }
-        });
-    });
-});
+            // example call
+            /* button  #btntrabalhos */
+            $(document).on("click", "#btnsincronizar", function (evt) {
+                    $.ajaxSetup({
+                        async: true
+                    }); // chamada é síncrona, estamos aguardando retorno
+
+
+                    db.findTrabalhoAll(function (trabalhos) {
+
+                            for (var i = 0; i < trabalhos.length; i++) {
+
+                                $.ajax({
+                                    type: 'GET',
+                                    // adaptar para sincronização
+                                    url: 'http://rasystems.esy.es/index.php/itrabalho/'+
+                                    trabalhos[i].codtra + '/' +
+                                    trabalhos[i].nomtra + '/' +
+                                    trabalhos[i].nomcur,
+
+                                    /*data: {
+                                        "id": trabalhos[i].codtra,
+                                        "nomtra": trabalhos[i].nomtra,
+                                        "nomcur": trabalhos[i].nomcur
+                                    },*/
+
+                                    dataType: 'json',
+                                    contentType: 'application/json; charset=utf-8',
+                                    beforeSend: function () {
+                                        $('#loading').show();
+                                    },
+                                    complete: function () {
+                                        //$("#loading").hide();
+                                    },
+                                    success: function (response) {
+                                        //alert(JSON.stringify(response));
+                                    },
+                                    error: function (error) {
+                                        console.log(error);
+                                    }
+                                });
+
+                            }
+
+                        }
+
+
+                    });
+            });
